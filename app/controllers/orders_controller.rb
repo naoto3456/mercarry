a27f78be539ebf89a71b5d84fd07faa72e0fd951
nhnaoto <nhnaoto.nh@gmail.com>
2017-05-29 10:23:20 +0800

    added flash feature

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,14 @@ class OrdersController < ApplicationController
 
 	def create
 		order = Order.new(listing_id: params[:listing_id], buyer_id: view_context.current_user.id, ordered_at: Time.now)
-		order.save
+		
+		if order.save
+			flash[:notice] = "your order succeed."
+			redirect_to listing_path(params[:listing_id])
+		else
+			flash.now[:notice] = "your order failed."
+			render listing_path(params[:listing_id])			
+		end
 	end
 
 
